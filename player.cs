e3da84e88d35e212fcdf91ef04621f1d2e5c4436
davidKH <davidjb06.kodehode@gmail.com>
2023-11-21 10:38:33 +0100

    Csharp work

---
+++ player.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Transform GroundCheckTransform = null;
+    [SerializeField] private LayerMask playermask;
+
+    private bool JumpKeyWasPressed;
+    private float HorizontalImput;
+    private Rigidbody RigidBodyComponent;
+    private int SuperJumpsRemaining;
+    private float SprintKeyWasPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RigidBodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if space key is pressed down for jump
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            JumpKeyWasPressed = true;
+        }
+
+        HorizontalImput = Input.GetAxis("Horizontal");
+    }
+    // fixed update is called once every pychics update
+    private void FixedUpdate()
+    {
+        RigidBodyComponent.velocity = new Vector3(HorizontalImput, RigidBodyComponent.velocity.y, 0);
+
+        if (Physics.OverlapSphere(GroundCheckTransform.position, 0.1f, playermask).Length == 0)
+        {
+            return;
+        }
+
+        if (JumpKeyWasPressed == true)
+        {
+            float JumpPower = 5f;
+            if (SuperJumpsRemaining > 0)
+            {
+                JumpPower *= 2;
+                SuperJumpsRemaining--;
+            }
+            RigidBodyComponent.AddForce(Vector3.up * JumpPower, ForceMode.VelocityChange);
+            JumpKeyWasPressed = false;
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 7)
+        {
+            Destroy(other.gameObject);
+            SuperJumpsRemaining += 1;
+        }
+
+    }
+
+}
---
+++ playercontroller.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+
+    [SerializeField] Transform PlayerCamera = null;
+    [SerializeField] float Mousesensetivity = 3.5f;
+    [SerializeField] float Walkspeed = 6.0f;
+    [SerializeField] float JumpHeight = 3f;
+    [SerializeField] float gravity = -13.0f;
+    [SerializeField][Range(0.0f, 0.5f)] float MoveSmoothTime = 0.3f;
+    [SerializeField][Range(0.0f, 0.5f)] float MouseSmoothTime = 0.03f;
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    public float speed = 10f;
+
+    public float jumpHeight = 5f;
+
+    [SerializeField] bool Lockedcursor = true;
+
+    float cameraPitch = 0.0f;
+    float velocityY = 0.0f;
+    CharacterController controller = null;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    Vector2 currentDir = Vector2.zero;
+    Vector2 currentDirvelocity = Vector2.zero;
+
+    Vector2 currentmousedelta = Vector2.zero;
+    Vector2 currentmousedeltavelocity = Vector2.zero;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        if (Lockedcursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+
+    void Update()
+    {
+        UpdateMouseLook();
+        updatemovement();
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+
+    }
+
+    void GroundCheck()
+    {
+        RaycastHit hit;
+        float distance = 1f;
+        Vector3 dir = new Vector3(0, -1);
+
+        if (Physics.Raycast(transform.position, dir, out hit, distance))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+    void UpdateMouseLook()
+    {
+        Vector2 targetmouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        currentmousedelta = Vector2.SmoothDamp(currentmousedelta, targetmouseDelta, ref currentmousedeltavelocity, MouseSmoothTime);
+
+
+        cameraPitch -= currentmousedelta.y * Mousesensetivity;
+
+        cameraPitch = Mathf.Clamp(cameraPitch, -90.0f, 90.0f);
+
+        PlayerCamera.localEulerAngles = Vector3.right * cameraPitch;
+
+        transform.Rotate(Vector3.up * currentmousedelta.x * Mousesensetivity);
+    }
+
+
+    void updatemovement()
+    {
+        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        targetDir.Normalize();
+
+        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirvelocity, MoveSmoothTime);
+
+        if (controller.isGrounded)
+            velocityY = 0.0f;
+
+        velocityY += gravity * Time.deltaTime;
+
+        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * Walkspeed + Vector3.up * velocityY;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
